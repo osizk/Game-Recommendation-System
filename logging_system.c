01024a57b8498575dbb99ffa1fbb20ec2a541329
NinjoMUDA <121446792+NinjoMUDA@users.noreply.github.com>
2025-04-26 20:29:42 +0700

    activity to event

---
+++ logging_system.c
@@ -0,0 +1,8 @@
+#include "logging_system.h"
+#include <stdio.h>
+#include <time.h>
+
+void logging_event(const char *event, const char *user)
+{
+
+}
--- logging_system.h
+++ logging_system.h
@@ -1,6 +1,6 @@
 #ifndef Logging_System_H
 #define Logging_System_H
 
-void logging_event(const char *activity, const char *user);
+void logging_event(const char *event, const char *user);
 
 #endif
